24203b73289e73785be2ebf368abc7a62415cd74
FreemoX <frarol96@gmail.com>
2021-02-22 13:56:54 +0100

    updated vesta installation script

--- bin/install-vesta.sh
+++ bin/install-vesta.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade && sudo apt install curl nginx -y && wait
+grep VERSION_ID= /etc/os-release
+echo "Highest release compatible: 18.10"
+read -s -p "Press any key to continue, or 'CTRL + C' to abort"
+sudo apt update && sudo apt upgrade && sudo apt install curl nginx -y && wait && sleep 1
 cd /tmp
-curl -O http://vestacp.com/pub/vst-install.sh && wait
-sduo bash ./vst-install.sh && wait && sleep 5
-sudo ufw enable
-sudo ufw allow 8083
+sudo curl -O http://vestacp.com/pub/vst-install.sh && wait && sleep 1
+#sduo bash ./vst-install.sh && wait && sleep 5
+bash vst-install.sh --nginx yes --apache yes --phpfpm no --named no --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban yes --quota no --exim yes --dovecot yes --spamassassin yes --clamav yes --softaculous yes --mysql yes --postgresql no --hostname localhost --email fr@norditc.no --password Algebra2154 && wait && sleep 5
+sudo ufw enable && wait
+sudo ufw allow 8083 && wait
 sudo ufw allow 'Nginx HTTP'
-sudo ufw allow 'Nginx HTTPS'
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+sudo ufw allow 'Nginx HTTPS' && wait && sleep 1
+sudo systemctl restart nginx && wait && sleep 2
+sudo systemctl enable nginx && wait
 echo ""
 echo "Testing localhost https at port 8083"
 curl -Is https://localhost:8083 | head -n 1 && wait
@@ -16,7 +20,7 @@ echo ""
 echo "Testing localhost http at port 8083"
 curl -Is http://localhost:8083 | head -n 1 && wait
 firefox -new-tab "https://localhost:8083"
-# echo << ENDOFECHO
+# 
 #                                    _,,---.)\__
 #                                  ,'.          ""`.
 #                                 f.:               \
@@ -41,4 +45,3 @@ firefox -new-tab "https://localhost:8083"
 #                       `;;;;;;;;;:);;;;:::::...          /\;;;;:j
 #                         "`------'-.;;;;::::::::...._,-'"  `---'
 #                                     ``"""""""""""''
-# ENDOFECHO
