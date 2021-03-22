ad9e45ac223c65f883de9828fd23754e84f3cd3b
FreemoX <frarol96@gmail.com>
2021-03-22 23:28:18 +0100

    Added auto-install and install-check functionality

--- bin/cmdtt.sh
+++ bin/cmdtt.sh
@@ -2,10 +2,17 @@
 cmdtt=""
 cmdttr="-1"
 cmdtt="$1"
-if ! command -v "$cmdtt" &> /dev/null
-then
-    echo ""$cmdtt" could not be found"
+if ! command -v "$cmdtt" &> /dev/null; then
+    echo ""
+    echo ""$cmdtt" could not be found, installing it"
+    echo ""
+    sudo apt update && sudo apt install "$cmdtt" -y && wait
     cmdttr="0"
+    if ! command -v "$cmdtt" &> /dev/null; then
+        echo ""
+        echo ""$cmdtt" has been installed"
+        echo ""
+        cmdttr="1"
 else
     echo ""$cmdtt" is installed"
     cmdttr="1"
